3a5e29e29315ffa32549190135efb2774550409c
joke3829 <144600340+joke3829@users.noreply.github.com>
2025-04-15 14:17:05 +0900

    1155656

--- ServerPractice/MultiThread/main.cpp
+++ ServerPractice/MultiThread/main.cpp
@@ -1,21 +1,46 @@
 #include <iostream>
 #include <thread>
 #include <vector>
+#include <chrono>
 #include <mutex>
 
 std::mutex sm;
 
-void worker()
+void worker(volatile int& sum)
 {
-	sm.lock();
-	std::cout << std::this_thread::get_id() << std::endl;
-	sm.unlock();
+	for (int i = 0; i < 50000000 / 2; ++i) {
+		sm.lock();
+		sum = sum + 2;
+		sm.unlock();
+	}
 }
 
 int main()
 {
-	std::vector<std::thread> my_thread;
-	for (int i = 0; i < 10; ++i)
-		my_thread.emplace_back(worker);
-	for (auto& t : my_thread) t.join();
+	using namespace std::chrono;
+
+	{
+		volatile int sum = 0;
+		auto start_t = high_resolution_clock::now();
+		for (int i = 0; i < 50000000; ++i)
+			sum = sum + 2;
+		auto end_t = high_resolution_clock::now();
+		auto exec_t = end_t - start_t;
+		auto ms = duration_cast<milliseconds>(exec_t).count();
+
+		std::cout << "SingleThread Sum : " << sum << ", Time - " << ms << "ms" << std::endl;
+	}
+
+	{
+		volatile int sum = 0;
+		auto start_t = high_resolution_clock::now();
+		std::thread t1{ worker , std::ref(sum)};
+		std::thread t2{ worker , std::ref(sum)};
+		t1.join(); t2.join();
+		auto end_t = high_resolution_clock::now();
+		auto exec_t = end_t - start_t;
+		auto ms = duration_cast<milliseconds>(exec_t).count();
+
+		std::cout << "2 Thread Sum : " << sum << ", Time - " << ms << "ms" << std::endl;
+	}
 }
